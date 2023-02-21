4096f5509f4e57c845bd3e5147e4b5ee64f6d875
re-beccalouise <re-beccalouise@outlook.com>
2023-02-21 15:34:17 +0000

    initial commit

---
+++ Elecshop.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Elecshopapi.Models {
+
+    public class ElectricalItem {
+
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public string? Description { get; set; }
+        public double? Price { get; set; }
+
+    }
+}
